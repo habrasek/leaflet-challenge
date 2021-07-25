358db152080549ff2705fd1121330e4d5e19fe9c
habrasek <65578198+habrasek@users.noreply.github.com>
2021-07-25 09:41:18 -0400

    markers work

--- logic.js
+++ logic.js
@@ -13,3 +13,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+let lats = [];
+let lons = [];
+
+d3.json(link).then(info =>{
+    console.log(info);
+    console.log(Object.keys(info));
+    let feat = info.features;
+    console.log(feat);
+    console.log(feat.length);
+    for(i=0;i<feat.length;i++){
+        lats.push(feat[i]["geometry"]["coordinates"][1]);
+        lons.push(feat[i]["geometry"]["coordinates"][0]);
+    }
+    console.log(lats);
+    console.log(lons);
+
+    for(i=0; i<lats.length; i++){
+    L.marker([lats[i], lons[i]])
+    .addTo(myMap)};
+}
+)
+
+
+
